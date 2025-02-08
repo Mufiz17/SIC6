7d90e71feacd305caac22b3034050d2bde1e2645
Firman-001 <pirman475@smk.belajar.id>
2025-02-08 11:14:43 +0700

    calculate area

---
+++ rectangle_area.py
@@ -0,0 +1,9 @@
+def calculate_area(width: float, length: float) -> float:
+    """Menghitung luas berdasarkan lebar dan panjang."""
+    return width * length
+
+# Contoh penggunaan
+width = 5
+length = 10
+area = calculate_area(width, length)
+print(f"Luas: {area}")
